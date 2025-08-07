1a44f238eca7c24bb1f9e85ca27d32508fc5d583
Snu Seam <siamahme766@gmail.com>
2025-08-07 13:28:45 +0600

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
@@ -9,9 +10,8 @@ app.use(cors())
 
 
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://snu_seam:IVb56JYXtSvpIaq8@cluster0.4rme0sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4rme0sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
